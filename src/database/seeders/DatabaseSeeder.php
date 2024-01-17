83b285bc028b4a6337af8c58f9ab22b9cd4988d2
syu.onoda <s-onoda@creative-innovation.co.jp>
2024-01-17 19:51:15 +0900

    add setting

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -1,22 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Testing\DatabaseTruncation;
 
-class DatabaseSeeder extends Seeder
+final class DatabaseSeeder extends Seeder
 {
+    use DatabaseTruncation;
+
+    public function __construct(protected readonly Application $app)
+    {
+    }
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->truncateTablesForAllConnections();
+        $this->call(UserSeeder::class);
     }
 }
